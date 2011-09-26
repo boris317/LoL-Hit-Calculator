00b0277b371e7256bf635800f739b41255bfb02a
Shawn Adams <shawn@mobiata.com>
2011-09-25 20:30:13 -0400

    Riven Hotfix. Finally updated

--- lol.js
+++ lol.js
@@ -1,68 +1,68 @@
 // DefensePenetration Object: Represents both magic and armor penetration/reduction.
 var DefensePenetration = function(initObject){
-	this.reduction = initObject['reduction'] || null;
-	this.reductionPercent = initObject['reductionPercent'] || null;
-	this.penetration = initObject['penetration'] || null;
-	this.penetrationPercent = initObject['penetrationPercent'] || null; 
+    this.reduction = initObject['reduction'] || null;
+    this.reductionPercent = initObject['reductionPercent'] || null;
+    this.penetration = initObject['penetration'] || null;
+    this.penetrationPercent = initObject['penetrationPercent'] || null; 
 }
 
 // DamageReduction Object: Represents both armor and magic resist.
 var DamageReduction = function(amount, champion) {
-	this.amount = amount;
-	this.champion = champion || null;
+    this.amount = amount;
+    this.champion = champion || null;
 }
 
 DamageReduction.prototype.mitigate = function(damage, defensePenetration) {
     var amount = this.amount;
     if (defensePenetration != undefined) {
-		if (defensePenetration.reductionPercent) {
-			amount = amount - (amount * defensePenetration.reductionPercent);
-		}
-		if (defensePenetration.reduction) {
-			amount = amount - defensePenetration.reduction;
-		}		
-		if (amount > 0 && defensePenetration.penetrationPercent) {
-			amount = amount - (amount * defensePenetration.penetrationPercent);
-			amount = amount < 0 ? 0 : amount;
-		}		
-		if (amount > 0 && defensePenetration.penetration) {
-			amount = amount - defensePenetration.penetration;
-			amount = amount < 0 ? 0 : amount;			
-		}					
-	}
- 
+        if (defensePenetration.reductionPercent) {
+            amount = amount - (amount * defensePenetration.reductionPercent);
+        }
+        if (defensePenetration.reduction) {
+            amount = amount - defensePenetration.reduction;
+        }		
+        if (amount > 0 && defensePenetration.penetrationPercent) {
+            amount = amount - (amount * defensePenetration.penetrationPercent);
+            amount = amount < 0 ? 0 : amount;
+        }		
+        if (amount > 0 && defensePenetration.penetration) {
+            amount = amount - defensePenetration.penetration;
+            amount = amount < 0 ? 0 : amount;			
+        }					
+    }
+
     return damage * this.multiplier(amount);
 }
 
 DamageReduction.prototype.applyHit = function(hit) {
-	// Accepts a Hit object instance and applies the damage. Returns
-	// the final modified damage value after damage reduction has been
-	// applied.
-	return this.mitigate(hit.amount, hit.defensePenetration);
+    // Accepts a Hit object instance and applies the damage. Returns
+    // the final modified damage value after damage reduction has been
+    // applied.
+    return this.mitigate(hit.amount, hit.defensePenetration);
 }
 
 DamageReduction.prototype.multiplier = function(amount) {
-	var amount = amount || this.amount;
-	if (amount >= 0) {
-    	return 100 / (100 + amount);
-    } else if (amount < 0) {
-    	return 1 + Math.abs(amount) / 100;
-	}
+    var amount = amount || this.amount;
+    if (amount >= 0) {
+        return 100 / (100 + amount);
+} else if (amount < 0) {
+    return 1 + Math.abs(amount) / 100;
+}
 }
 
 DamageReduction.prototype.effectiveHealth = function(health) {
-	var health = health || 0;
-	if (this.champion) {
-		health = this.champion.getStat("health");
-	}
-	if (health == 0){
-		return 0;
-	}
-	if (this.amount >= 0) {
-		return health * (100 + this.amount) / 100;
-	} else if (this.amount) {
-		return health * 100 / (100 + Math.abs(this.amount));
-	}
+    var health = health || 0;
+    if (this.champion) {
+        health = this.champion.getStat("health");
+    }
+    if (health == 0){
+        return 0;
+    }
+    if (this.amount >= 0) {
+        return health * (100 + this.amount) / 100;
+    } else if (this.amount) {
+        return health * 100 / (100 + Math.abs(this.amount));
+    }
 }
 
 // Hit Object: This object represents a single hit or strike to a target. Hits are used
@@ -70,127 +70,127 @@ DamageReduction.prototype.effectiveHealth = function(health) {
 // applied. Hits are applied to a ``DamageReduction`` object via the ``DamageReduction.applyHit`` method.
 
 var Hit = function(amount, damageType, defensePenetration) {
-	// @param damageType - Magic or physical.
-	// @param amount - Amount of damage.
-	// @param defensePenetration - DefensePenetration object.
-	
-	this.amount = amount;
-	this.damageType = damageType;
-	this.defensePenetration = defensePenetration;
+    // @param damageType - Magic or physical.
+    // @param amount - Amount of damage.
+    // @param defensePenetration - DefensePenetration object.
+
+    this.amount = amount;
+    this.damageType = damageType;
+    this.defensePenetration = defensePenetration;
 }
 
 // Champion Object: Simple object to represent a champion.
 var Champion = function(champName){
-	this.name = champName;
-	this.baseStats = new Stats();
-	// If you want to add additional stats from items use the statsFromItems property.
-	this.statsFromItems = new Stats();	
-	this._level = 0;
-	
-	this.armorFromItems = 0;
-	this.magicResistFromItems = 0;
-	this.healthFromItems = 0;
+    this.name = champName;
+    this.baseStats = new Stats();
+    // If you want to add additional stats from items use the statsFromItems property.
+    this.statsFromItems = new Stats();	
+    this._level = 0;
+
+    this.armorFromItems = 0;
+    this.magicResistFromItems = 0;
+    this.healthFromItems = 0;
 }
 
 Champion.prototype.getStat = function(statName) {
-	var stat = this.baseStats.get(statName, this._level) + this.statsFromItems.get(statName);
-	if (statName == "armor" || statName == "magicResist") {
-		return new DamageReduction(stat, this);
-	}
-	return stat;
+    var stat = this.baseStats.get(statName, this._level) + this.statsFromItems.get(statName);
+    if (statName == "armor" || statName == "magicResist") {
+        return new DamageReduction(stat, this);
+    }
+    return stat;
 }
 
 Champion.prototype.level = function(){
-	return this._level + 1;
+    return this._level + 1;
 }
 
 Champion.prototype.setLevel = function(level) {
-	if (!(level <= 18 && level >= 1)) {
-		throw RangeError;
-	}
-	this._level = level - 1;
+    if (!(level <= 18 && level >= 1)) {
+        throw RangeError;
+    }
+    this._level = level - 1;
 }
 
 // championFactory creates Champion instances from champion stat objects in champs.js
 function championFactory(champName, championObject){
-	champion = new Champion(champName);
-	
-	function setter(statName, valueObject){
-		// Sets base stats on a Champion object instance.
-		champion.baseStats.set(statName, valueObject.value, valueObject.perLevel);
-	}	
-	
-	setter("health", parsePerLevelInc(championObject["health"]));
-	setter("attackDamage", parsePerLevelInc(championObject["damage"]));
-	setter("armor", parsePerLevelInc(championObject["armor"]));
-	setter("magicResist", parsePerLevelInc(championObject["magic res."]));
-	setter("mana", parsePerLevelInc(championObject["mana"]));
-	return champion;
+    champion = new Champion(champName);
+
+    function setter(statName, valueObject){
+        // Sets base stats on a Champion object instance.
+        champion.baseStats.set(statName, valueObject.value, valueObject.perLevel);
+    }	
+
+    setter("health", parsePerLevelInc(championObject["health"]));
+    setter("attackDamage", parsePerLevelInc(championObject["attack damage"]));
+    setter("armor", parsePerLevelInc(championObject["armor"]));
+    setter("magicResist", parsePerLevelInc(championObject["magic res."]));
+    setter("mana", parsePerLevelInc(championObject["mana"]));
+    return champion;
 }
 
 var perLevelRegex = /^([^\s]*)\s\(\+\s?([^\)]*)\)$/;
 function parsePerLevelInc(value){
-	var result = {
-		value: parseFloat(value),
-		perLevel:0
-	};
-	if (value == null || value == undefined) {
-		return result;
-	}
-	m = value.match(perLevelRegex);
-	if (!m) {
-		return result
-	} else {
-		result.value = parseFloat(m[1]);
-		result.perLevel = parseFloat(m[2]);
-	}
-	return result;
+    var result = {
+        value: parseFloat(value),
+        perLevel:0
+    };
+    if (value == null || value == undefined) {
+        return result;
+    }
+    m = value.match(perLevelRegex);
+    if (!m) {
+        return result
+    } else {
+        result.value = parseFloat(m[1]);
+        result.perLevel = parseFloat(m[2]);
+    }
+    return result;
 }
 
 // Stat objects
 var Stat = function(statName, value, perLevel) {
-	this.name = statName;
-	this.value = value;
-	this.perLevel = perLevel || 0;
+    this.name = statName;
+    this.value = value;
+    this.perLevel = perLevel || 0;
 }
 
 var Stats = function(){
-	this.stats = {};
+    this.stats = {};
 }
 
 Stats.prototype.set = function(statName, value, perLevel) {
-	this.stats[statName] = new Stat(statName, value, perLevel);
+    this.stats[statName] = new Stat(statName, value, perLevel);
 }
 
 Stats.prototype.get = function(statName, champLevel) {
-	var stat = this.stats[statName];
-	if (!stat) {
-		return 0;
-	}
-	return stat.value + ((champLevel || 0) * stat.perLevel);
+    var stat = this.stats[statName];
+    if (!stat) {
+        return 0;
+    }
+    return stat.value + ((champLevel || 0) * stat.perLevel);
 }
 
 // Rune objects
 var Runes = function() {
-	this.runes = []
+    this.runes = []
 }
 
 Runes.prototype.add = function(rune) {
-	this.runes.push(rune)
+    this.runes.push(rune)
 }
 
 Runes.prototype.getStat = function(statName, champLevel) {
-	statValue = 0;
-	for (index in this.runes) {
-		statValue = statValue + this.runes[index].stats.get(statName, champLevel);
-	}
-	return statValue;
+    statValue = 0;
+    for (index in this.runes) {
+        statValue = statValue + this.runes[index].stats.get(statName, champLevel);
+    }
+    return statValue;
 }
 
 var Rune = function(runeType, runeName, statName, statValue, perLevel, isPrimary){
-	this.type = runeType;
-	this.name = runeName;
-	this.isPrimary = isPrimary || false;
-	this.stats = Stat();
-	this.stats.set(statName, statValue, perLevel || 0);
+    this.type = runeType;
+    this.name = runeName;
+    this.isPrimary = isPrimary || false;
+    this.stats = Stat();
+    this.stats.set(statName, statValue, perLevel || 0);
 }
